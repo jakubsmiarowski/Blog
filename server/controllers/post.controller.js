15a9481533701d763482fa8621d9ffb10671417a
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-09-21 19:56:54 +0200

    add mongoDB and Mongoose features

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,12 @@
+const Post = require('../models/post.model');
+
+// get all posts
+
+exports.getPosts = async(req, res) => {
+
+    try {
+        res.status(200).json(await Post.find());
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
