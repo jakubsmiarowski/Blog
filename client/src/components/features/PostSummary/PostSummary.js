d72b43e89d83e0656c9b076a31067d0213e2ddfc
Jakub Smiarowski <jakub.smiarowski@gmail.com>
2019-09-26 19:52:23 +0200

    31.1 updated, added author in PropTypes

--- client/src/components/features/PostSummary/PostSummary.js
+++ client/src/components/features/PostSummary/PostSummary.js
@@ -8,13 +8,14 @@ import SmallTitle from '../../common/SmallTitle/SmallTitle';
 import HtmlBox from "../../common/HtmlBox/HtmlBox";
 import cutText from '../../../cutText/cutText';
 
-const PostSummary = ({ id, title, content }) => (
+const PostSummary = ({ id, title, content, author }) => (
   <article className="post-summary">
     <SmallTitle>{title}</SmallTitle>
     <HtmlBox>{cutText(content, 250)}</HtmlBox>
     <Button variant="primary">
     <Link to={`/posts/${id}`}>Read more</Link>  
     </Button>
+    <p>Author: {author}</p>
   </article>
 );
 
@@ -22,6 +23,7 @@ PostSummary.propTypes = {
   id: PropTypes.string,
   title: PropTypes.string,
   content: PropTypes.string,
+  author: PropTypes.string,
 };
 
 export default PostSummary;
