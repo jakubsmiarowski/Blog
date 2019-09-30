800914cf71162a8aab2d86f9b0de17001f3ff2f5
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2019-09-30 16:05:16 +0200

    fix posts.js

--- client/src/components/features/Posts/Posts.js
+++ client/src/components/features/Posts/Posts.js
@@ -1,47 +1,49 @@
-import React from 'react';
-import { PropTypes } from 'prop-types';
-import PostsList from '../PostsList/PostsList';
-import Spinner from '../../common/Spinner/Spinner';
-import Alert from '../../common/Alert/Alert';
-import Pagination from '../../common/Pagination/Pagination';
-
+import React from "react";
+import { PropTypes } from "prop-types";
+import PostsList from "../PostsList/PostsList";
+import Spinner from "../../common/Spinner/Spinner";
+import Alert from "../../common/Alert/Alert";
+import Pagination from "../../common/Pagination/Pagination";
 
 class Posts extends React.Component {
-
   componentDidMount() {
     const { loadPostsByPage, postsPerPage, initialPage } = this.props;
     loadPostsByPage(initialPage || 1, postsPerPage);
   }
 
-  loadPostsPage = (page) => {
+  loadPostsPage = page => {
     const { loadPostsByPage, postsPerPage } = this.props;
     loadPostsByPage(page, postsPerPage);
-  }
+  };
 
   render() {
-    const {posts, request, pages, pagination, presentPage} = this.props;
+    const { posts, request, pages, pagination, presentPage } = this.props;
     const { loadPostsPage } = this;
 
-    const textin =  request.pending ? ( 
-        <Spinner /> 
-      ) : request.success ? ( 
-         posts.length > 0 ? (
-          <div>
-            <PostsList posts={posts} />
-            {pagination && <Pagination pages={pages} initialPage={presentPage} onPageChange={loadPostsPage} />}
-          </div>
-      ) : ( 
+    const textin = request.pending ? (
+      <Spinner />
+    ) : request.success ? (
+      posts.length > 0 ? (
+        <div>
+          <PostsList posts={posts} />
+          {pagination && (
+            <Pagination
+              pages={pages}
+              initialPage={presentPage}
+              onPageChange={loadPostsPage}
+            />
+          )}
+        </div>
+      ) : (
         <Alert variant="info"> No posts!!! </Alert>
-      )) : ( 
-        <Alert variant="error"> {request.error} </Alert>
-      );
-
-    return (
-      <div> {textin} </div>
+      )
+    ) : (
+      <Alert variant="error"> {request.error} </Alert>
     );
-  }
 
-};
+    return <div> {textin} </div>;
+  }
+}
 
 Posts.propTypes = {
   posts: PropTypes.arrayOf(
@@ -50,10 +52,10 @@ Posts.propTypes = {
       title: PropTypes.string.isRequired,
       content: PropTypes.string.isRequired,
       author: PropTypes.string.isRequired,
-      pages: PropTypes.number.isRequired,
+      pages: PropTypes.number.isRequired
     })
   ),
-  loadPostsByPage: PropTypes.func.isRequired,
+  loadPostsByPage: PropTypes.func.isRequired
 };
 
 export default Posts;
--- client/src/components/features/Posts/PostsContainer.js
+++ client/src/components/features/Posts/PostsContainer.js
@@ -1,15 +1,24 @@
-import { connect } from 'react-redux';
-import { getPosts, getRequest, loadPostsByPageRequest, getPages } from '../../../redux/postsRedux';
-import Posts from './Posts';
+import { connect } from "react-redux";
+import {
+    getPosts,
+    getRequest,
+    loadPostsByPageRequest,
+    getPages
+} from "../../../redux/postsRedux";
+import Posts from "./Posts";
 
 const mapStateToProps = state => ({
     posts: getPosts(state),
     request: getRequest(state),
-    pages: getPages(state),
+    pages: getPages(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-    loadPostByPage: (page, postsPerPage) => dispatch(loadPostsByPageRequest(page, postsPerPage)),
+    loadPostsByPage: (page, postsPerPage) =>
+        dispatch(loadPostsByPageRequest(page, postsPerPage))
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Posts);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Posts);
